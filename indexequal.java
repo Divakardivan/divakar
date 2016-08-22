a9ce557f0b6dddaedf264671801aab3c583176cf
Divakardivan <divandivakar555@gmail.com>
2016-08-22 19:03:03 +0530

    Create indexequal.java

---
+++ indexequal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class indexequal
+{
+public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in);
+ int n=sc.nextInt();
+ int[] arr=new int[n];
+ for(int i=0;i<n;i++)
+ {
+    arr[i]=sc.nextInt();
+    if(arr[i]==i)
+       System.out.println(arr[i]);
+ }
+ }
+}
