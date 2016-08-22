b27744518b05d56a45bbaba8c10f9d8c7c613837
Divakardivan <divandivakar555@gmail.com>
2016-08-22 19:27:22 +0530

    Update unsortedarray.java

--- unsortedarray.java
+++ unsortedarray.java
@@ -10,7 +10,7 @@ public static void main(String[] args)
     arr[i]=sc.nextInt();
   Arrays.sort(arr);
     for(int i=0;i<n-1;i++)
-       if(!arr[i]==arr[i+1])
+       if(arr[i]!=arr[i+1])
        {
           System.out.println(arr[i+1]);
           break;
