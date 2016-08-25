27f78801f9df045202fa7b09f25812ad0fab92a1
Divakardivan <divandivakar555@gmail.com>
2016-08-25 10:21:15 +0530

    Update sumofint.java

--- sumofint.java
+++ sumofint.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
   Scanner sc=new Scanner(System.in);
   int n=sc.nextInt();
   String s="";
-  s+=n;
+  s+=Math.abs(n);
   String ch="";
   int sum=0;
   for(int i=0;i<s.length();i++)
